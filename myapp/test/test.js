5e323c55c8ddf9ca561ca6d3161153c0de0b73ba
rogerthatdev <roger.that.dev@gmail.com>
2023-04-11 16:55:00 -0700

    add a GET / test

--- myapp/test/test.js
+++ myapp/test/test.js
@@ -1,4 +1,6 @@
 var assert = require('assert');
+const request = require('supertest');
+const app = require('../app.js')
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -6,3 +8,12 @@ describe('Array', function () {
     });
   });
 });
+
+describe('GET /', function() {
+  it('return a 200 response', function(){
+    return request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type',/html/)
+  })
+})
