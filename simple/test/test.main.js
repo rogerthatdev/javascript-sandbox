45d27a584a720957b8d14691d3d39a6c24fe9d1d
rogerthatdev <roger.that.dev@gmail.com>
2023-04-11 17:45:35 -0700

    add super simple example with tests

---
+++ simple/test/test.main.js
@@ -0,0 +1,13 @@
+var assert = require('assert');
+const app = require('../main.js')
+
+describe('things for strings', function () {
+  describe('reverse a string', function () {
+    it('should return olleh', function () {
+      assert.equal(app('hello', 'reverse'), 'olleh');
+    });
+    it('should return -1 for missing actions', function () {
+        assert.equal(app('hello', 'nonexistent'), -1);
+      });
+  });
+});
