aab5fa59d07b62f2a642de689f483f01d3b33068
rogerthatdev <roger.that.dev@gmail.com>
2023-04-11 18:10:11 -0700

    add simple array code with tests

---
+++ simple/arrays.js
@@ -0,0 +1,11 @@
+
+var app = (array, action, param = null) => {
+    if(action === "trib") {
+        return [1,1,1,3,5,9,17,31,57,105]
+    }
+    else {
+        return -1
+    }
+}
+
+module.exports = app;
--- simple/test/test.main.js
+++ simple/test/test.main.js
@@ -1,5 +1,6 @@
 var assert = require('assert');
 const app = require('../main.js')
+const arrays = require('../arrays.js')
 
 describe('things for strings', function () {
   describe('reverse a string', function () {
@@ -8,8 +9,22 @@ describe('things for strings', function () {
     });
   });
   describe('missing actions for strings', function (){
-    it('should return -1 for missing actions', function () {
+    it('should return -1 for missing string actions', function () {
         assert.equal(app('hello', 'nonexistent'), -1);
       });
   })
 });
+
+describe('things for arrays', function () {
+    describe('tribonacci', function () {
+      it('should return trib sequence', function () {
+        assert.equal(arrays([1,1,1], 'trib', 10).toString(), [1,1,1,3,5,9,17,31,57,105].toString());
+      });
+    });
+    describe('missing actions for arrays', function () {
+      it('should return -1 for missing array actions', function () {
+          assert.equal(arrays([], 'nonexistent'), -1);
+        });
+    });
+});
+
